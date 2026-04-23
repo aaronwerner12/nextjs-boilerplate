6d293be64842530ce83278d6d16cee8ddf289eb4
aaronwerner12 <aaronwerner12@gmail.com>
2026-04-23 15:09:40 -0500

    Update ETFPlaybook.jsx

--- app/ETFPlaybook.jsx
+++ app/ETFPlaybook.jsx
@@ -1,14 +1,20 @@
 "use client";
+
 import { useState } from "react";
 
 export default function ETFPlaybook() {
-  const [count, setCount} = useState(0);
+const [count, setCount] = useState(0);
 
-return (
-  <div>
-  <h1>ETF Playbook</h1>h1>
-  <button onClick={() => setcount(count +1)}>Add Event</button>button>
-  <p>Count: {count}</p>p>
-  </div>
-  );
+return ( <div> <h1>ETF Playbook</h1>
+
+```
+  <button onClick={() => setCount(count + 1)}>
+    Add Event
+  </button>
+
+  <p>Count: {count}</p>
+</div>
+```
+
+);
 }
